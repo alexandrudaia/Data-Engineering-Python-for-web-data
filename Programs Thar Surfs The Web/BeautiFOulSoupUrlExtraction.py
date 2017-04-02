cc5b045780e68cc4fa45849c2c5a40352ea9da4f
root <root@ubuntu.ubuntu-domain>
2017-04-02 18:55:33 +0300

    added scrapper for  getting   all urls  from  web page ,  this  good  for my  kali linux   hacking   spaming

---
+++ Programs Thar Surfs The Web/BeautiFOulSoupUrlExtraction.py
@@ -0,0 +1,62 @@
+import  BeautifulSoup
+import urllib
+url=raw_input('Enter -')
+url=raw_input('Enter -')
+html=urllib.urlopen(url).read()
+url=raw_input('Enter -')
+html=urllib.urlopen(url).read()
+html=urllib.urlopen(url).read()
+with urllib.request.urlopen(url)  as url:
+    s=url.read()
+import  urrlib.request
+import  urllib.request
+import urllib.request
+s=urllib.urlopen(url)
+url
+url[7:]
+url=url[7:]
+html=urrlib.urlopen(url).read()
+html=urllib.urlopen(url).read()
+html
+#retrieve a list of the  anchor tags
+#Each  tag  is  like  a dictionary of HTML attribues
+soup=BeautifulSoup(html)
+html
+from BeautifulSoup import *
+BeautifulSoup(html)
+soup=BeautifulSoup(html)
+soup
+url
+tags=soup('a')
+rags
+tags
+for  tag in tags :
+    print(tag.get('href',None)
+    
+    )
+html
+soup
+tags=h1
+tahgs='h1'
+tags='h1'
+for tag in tags:
+    print(tag.text)
+for tag in tags:
+    print(tag.get('text','None'))
+tags
+tags=soup('h1')
+tags
+for  tag in  tags:
+    print(tag.text)
+url="http:www/antena3.ro"
+url="http://www.antena3.ro"
+html=urrlib.urlopen(url).read()
+html=urllib.urlopen(url).read()
+html
+soup=BeautifulSoup(html)
+soup
+tags=soup('a')
+for tag in tags:
+    print(tag.get('href',None))
+#Observtion  : this will  be  good  when I work on  kali  linux
+%history  -f BeautiFOulSoupUrlExtraction.py
